7a6a6b2a9f0617c0c6e8d3ece95daaa20ad9c8f4
chaub123 <kinchau@hotmail.com.hk>
2024-01-08 23:43:08 +0800

    Update

--- Udemy/src/Chapter2/exercise2.java
+++ Udemy/src/Chapter2/exercise2.java
@@ -3,6 +3,15 @@ package Chapter2;
 // Udemy threading class ch2.5
 public class exercise2 {
     public static void main(String[] args) {
+        Thread thread = new newThread();
+        thread.setName("Testing 1");
+        thread.start();
+    }
 
+    public static class newThread extends Thread{
+        @Override
+        public void run(){
+            System.out.println("Hello from " + this.getName());
+        }
     }
 }
